bd732561e3c828dbcd64fc7eb2042e40e77bc039
DESKTOP-O709VJ6\Justin <ocamsze@gmail.com>
2022-06-12 22:13:40 -0400

    added more programs

---
+++ hours.py
@@ -0,0 +1,9 @@
+#Name: Justin K Too
+#Email: justin.too27@myhunter.cuny.edu
+#This program takes hours userinput and displays days til the weekend
+
+userInput = int(input(""))
+days = int(userInput / 24)
+hours = userInput % 24
+print("Days: ", days)
+print("Hours: ", hours)
---
+++ kmtoMiles.py
@@ -0,0 +1,8 @@
+#Name: Justin K Too
+#Email: justin.too27@myhunter.cuny.edu
+#This program converts kilometers to miles
+
+userInput = int(input("Enter kilometers: "))
+
+miles = 0.621371 * userInput
+print(miles)
